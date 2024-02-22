fe6b92c8d8988f79564cd714e52792415b21cc1e
Keisyeliz <158522128+Keisyeliz@users.noreply.github.com>
2024-02-22 17:42:01 -0500

    Add files via upload

---
+++ ejemplo2.py
@@ -0,0 +1,6 @@
+numeroInt = int(input('ingrese un número'))
+
+if numeroInt >= 1 and numeroInt <= 10:
+    print('Adivinaste el rango del número')
+else:
+    print('No adivinaste el rango del número')
